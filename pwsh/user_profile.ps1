dd2d826ecedec80ce57b390996cd365deb084f30
Tam Thai <hoangtam3062002@gmail.com>
2026-01-08 21:52:15 +0100

    add eza to replace ls

--- pwsh/user_profile.ps1
+++ pwsh/user_profile.ps1
@@ -1,18 +1,21 @@
-# Alias
-Set-Alias ll ls
-Set-Alias grep findstr
+Set-Alias ls li
+Set-Alias grep rg
 Set-Alias g git
 Set-Alias touch New-Item
 Set-Alias which where.exe
+
+function li {eza -A -G --icons --group-directories-first}
+function la {eza -lah --icons --group-directories-first}
+function ll {eza -l --icons --group-directories-first}
+
 function ln ($target, $link) {
     New-Item -ItemType SymbolicLink -Path $link -Value $target
 }
-# function sudo ($command) {
-#     Start-Process pwsh "-noe -c $command" -Verb Runas
-# }
+
 function rename ($target, $link) {
     Rename-Item $target $link
 }
+
 function time {
     Param(
         [Parameter(Mandatory=$true)]
@@ -29,4 +32,14 @@ function time {
     } finally {
         $(Get-Date) - $start
     }
+}
+
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi.exe $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
 }
